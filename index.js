77918b0636aab9d8cd4e94f3fcae2faebde5958e
Franziska <Franziska.Roder@gmx.de>
2025-01-28 16:34:28 +0100

    getComputerChoice

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log("hello world");
+// console.log("hello world");
+
+getComputerChoice()
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+
+
+    if (randomNumber <= (1/3)) {
+        console.log('rock');
+    }
+  
+    if (randomNumber >= (1/3) && randomNumber <= (2/3)) {
+        console.log('paper');
+    }
+
+    if (randomNumber >= (2/3)) {
+        console.log('scissors');
+    }
+
+    console.log(randomNumber);
+    
+    
+}

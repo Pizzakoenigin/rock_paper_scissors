e3838879428e6de8f664ed310cc82f7845fbc99d
Franziska <Franziska.Roder@gmx.de>
2025-02-04 15:00:07 +0100

    gamelogic re worked

--- index.js
+++ index.js
@@ -1,74 +1,49 @@
 'use strict';
 
-playGame();
+initGame();
 
-function getComputerChoice(userChoice, computerChoice, gameMoves,) {
+function getComputerChoice(userChoice, computerChoice, gameMoves, gameData) {
     let computerChoiceLabel = document.createElement("p")
     let randomNumber = Math.floor(Math.random() * (Object.keys(gameMoves).length));
     computerChoice = Object.keys(gameMoves)[randomNumber];
     document.querySelector(".userInputText").appendChild(computerChoiceLabel).textContent = `The computer used ${computerChoice}`
-    
-    playRound(userChoice, computerChoice, gameMoves)
+    playRound(userChoice, computerChoice, gameMoves, gameData)
 }
 
 
 
-function getHumanChoice(userChoice, computerChoice, gameMoves) {
-
-    let rockButton = document.createElement("button")
-    let paperButton = document.createElement("button")
-    let scissorsButton = document.createElement("button")
-    let dragonButton = document.createElement("button")
-    let ufoButton = document.createElement("button")
-    let humanChoiceLabel = document.createElement("p")
-    humanChoiceLabel.classList.add('userInputText')
-    let gamefield = document.querySelector("body")
-
-    gamefield.appendChild(rockButton).textContent = "Rock 🪨";
-    gamefield.appendChild(paperButton).textContent = "Paper 📄";
-    gamefield.appendChild(scissorsButton).textContent = "Scissors ✂️"
-    gamefield.appendChild(dragonButton).textContent = "Dragon 🐉"
-    gamefield.appendChild(ufoButton).textContent = "Ufo 🛸"
-
-    let buttons = gamefield.querySelectorAll("button")
-    buttons.forEach((button) => {
-        button.addEventListener("click", () => {
-            gamefield.appendChild(humanChoiceLabel).textContent = `You used ${button.textContent}`
-            userChoice = button.textContent.toLowerCase().slice(0, button.textContent.length - 3)
-            getComputerChoice(userChoice, computerChoice, gameMoves)
-            // return (button.textContent)
-        })
-    })
-}
-
-
-function playRound(userChoice, computerChoice, gameMoves) {
-    
-    let gamefield = document.querySelector("body")
-    let resultLabel = document.createElement("p")
-
+function playRound(userChoice, computerChoice, gameMoves, gameData) {
     if (gameMoves[userChoice].wins.includes(computerChoice)) {
-        gamefield.appendChild(resultLabel).textContent = `you won :) ${userChoice} beats ${computerChoice}`
-        humanScore++;
+        document.querySelector(".finalResult").textContent = `you won :) ${userChoice} beats ${computerChoice}`
+        gameData.humanScore++;
     } else if (gameMoves[userChoice].loses.includes(computerChoice)) {
-        gamefield.appendChild(resultLabel).textContent = `you lost :( ${computerChoice} beats ${userChoice}`
-        computerScore++;
+        document.querySelector(".finalResult").textContent = `you lost :( ${computerChoice} beats ${userChoice}`
+        gameData.computerScore++;
     } else if (gameMoves[userChoice].tie.includes(computerChoice)) {
-        gamefield.appendChild(resultLabel).textContent = 'tie!'
+        document.querySelector(".finalResult").textContent = 'tie!'
     }
 
-    round++
+    gameData.round++
+
+    if (gameData.round == 4) {
+        if (gameData.humanScore > gameData.computerScore) {
+            document.querySelector(".finalResult").textContent = `you won by ${gameData.humanScore} to ${gameData.computerScore}`;
+        } else if (gameData.humanScore < gameData.computerScore) {
+            document.querySelector(".finalResult").textContent = `you lost by ${gameData.computerScore} to ${gameData.humanScore}`;
+        } else if (gameData.humanScore == gameData.computerScore) {
+            document.querySelector(".finalResult").textContent = `tie`;
+        }
+    }
 }
 
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    let rounds = 5
-    let round = 0
-    let userChoice = ''
-    let computerChoice = ''
-
+function initGame() {
+    let gameData = {
+        humanScore: 0,
+        computerScore: 0,
+        rounds: 5,
+        round: 0
+    }
 
     const gameMoves = {
         'rock': { wins: ['scissors'], loses: ['paper', 'dragon', 'ufo'], tie: ['rock'] },
@@ -78,16 +53,33 @@ function playGame() {
         'ufo': { wins: ['scissors', 'rock', 'paper'], loses: ['dragon'], tie: ['ufo'] },
     };
 
-    if (round == 4) {
-        if (humanScore > computerScore) {
-            console.log(`you won by ${humanScore} to ${computerScore}`);
-        } else if (humanScore < computerScore) {
-            console.log(`you lost by ${computerScore} to ${humanScore}`);
-        } else if (humanScore == computerScore) {
-            console.log(`tie`);
-        }
-    }
+    let userChoice = ''
+    let computerChoice = ''
+    let gamefield = document.querySelector("body")
+    let resultLabel = document.createElement("p")
+    let rockButton = document.createElement("button")
+    let paperButton = document.createElement("button")
+    let scissorsButton = document.createElement("button")
+    let dragonButton = document.createElement("button")
+    let ufoButton = document.createElement("button")
+    let humanChoiceLabel = document.createElement("p")
 
-    getHumanChoice(userChoice, computerChoice, gameMoves)
+    gamefield.appendChild(rockButton).textContent = "Rock 🪨";
+    gamefield.appendChild(paperButton).textContent = "Paper 📄";
+    gamefield.appendChild(scissorsButton).textContent = "Scissors ✂️"
+    gamefield.appendChild(dragonButton).textContent = "Dragon 🐉"
+    gamefield.appendChild(ufoButton).textContent = "Ufo 🛸"
 
+    gamefield.appendChild(resultLabel)
+    humanChoiceLabel.classList.add('userInputText')
+    resultLabel.classList.add('finalResult')
+
+    let buttons = gamefield.querySelectorAll("button")
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            gamefield.appendChild(humanChoiceLabel).textContent = `You used ${button.textContent}`
+            userChoice = button.textContent.toLowerCase().slice(0, button.textContent.length - 3)
+            getComputerChoice(userChoice, computerChoice, gameMoves, gameData)
+        })
+    })
 }
